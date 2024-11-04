189afb1e1cd73e832e742cce6023a7e7fb0f1e22
James Munns <james@onevariable.com>
2024-11-04 17:42:52 +0100

    Add CLI and sdk

---
+++ tools/poststation-cli/src/main.rs
@@ -0,0 +1,329 @@
+use std::{collections::HashSet, net::SocketAddr, time::Instant};
+
+use anyhow::bail;
+use clap::{command, Args, Parser, Subcommand};
+use postcard_rpc::host_client::SchemaReport;
+use poststation_sdk::{
+    connect,
+    schema::schema::{
+        fmt::{discover_tys, is_prim},
+        owned::{OwnedDataModelType, OwnedNamedType},
+    },
+    SquadClient,
+};
+
+/// The Poststation CLI
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// A path to the server. Defaults to `127.0.0.1:51837`.
+    #[arg(short, long, value_name = "SERVER_ADDR")]
+    server: Option<SocketAddr>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    #[arg(long)]
+    timings: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List devices
+    Ls,
+
+    /// Endpoints of a given device
+    Endpoints { serial: String },
+
+    /// Get information about a device
+    Device(Device),
+    Proxy {
+        #[arg(short, long, value_name = "SERIAL")]
+        serial: String,
+        #[arg(short, long, value_name = "PATH")]
+        path: String,
+        #[arg(short, long, value_name = "MSG_JSON")]
+        message: String,
+    },
+}
+
+#[derive(Args)]
+struct Device {
+    serial: String,
+    #[command(subcommand)]
+    command: DeviceCommands,
+}
+
+#[derive(Subcommand)]
+enum DeviceCommands {
+    /// View all types used for communicating with a given device
+    Types,
+    /// View all endpoints available for communicating with a given device
+    Endpoints,
+    /// View all topics published by a given device
+    TopicsOut,
+    /// View all topics handled by a given device
+    TopicsIn,
+    /// View the most recent logs from a given device
+    Logs { count: Option<u32> },
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    let start = Instant::now();
+    let timings = cli.timings;
+    inner_main(cli).await?;
+    if timings {
+        println!("{:?}", start.elapsed());
+    }
+    Ok(())
+}
+
+async fn inner_main(cli: Cli) -> anyhow::Result<()> {
+    let server = cli
+        .server
+        .unwrap_or_else(|| "127.0.0.1:51837".parse().unwrap());
+
+    let Some(command) = cli.command else {
+        return Ok(());
+    };
+    let client = connect(server).await;
+
+    match command {
+        Commands::Ls => {
+            let devices = client.get_devices().await.unwrap();
+            println!();
+            println!("# Devices");
+            println!();
+            println!("| serial           | name       | interface | connected |");
+            println!("| :--------------- | ---------: | :-------- | :-------- |");
+            for dev in devices.iter() {
+                let ser = format!("{:016X}", dev.serial);
+                let conn = if dev.is_connected { "yes" } else { "no " };
+                println!("| {ser} | {:>10} | {:<9} | {conn:<9} |", dev.name, "usb");
+            }
+            println!();
+            Ok(())
+        }
+        Commands::Device(d) => device_cmds(client, &d).await,
+        Commands::Proxy {
+            serial,
+            message,
+            path,
+        } => device_proxy(client, serial, path, message).await,
+        Commands::Endpoints { mut serial } => {
+            serial = serial.to_uppercase();
+            let mut serial_num = None;
+            let mut serial_fragment = false;
+
+            if let Ok(ser) = u64::from_str_radix(&serial, 16) {
+                if serial.len() == 16 {
+                    serial_num = Some(ser);
+                } else {
+                    serial_fragment = true;
+                }
+            }
+
+            if serial_num.is_none() {
+                let devices = client.get_devices().await.unwrap();
+                let uppy = serial.to_uppercase();
+                let matches = devices
+                    .iter()
+                    .filter(|d| {
+                        d.name.contains(&uppy)
+                            || (serial_fragment && {
+                                let this_ser = format!("{:016X}", d.serial);
+                                this_ser.contains(&serial)
+                            })
+                    })
+                    .collect::<Vec<_>>();
+
+                if matches.is_empty() {
+                    bail!("Failed to find device matching '{serial}'");
+                } else if matches.len() > 1 {
+                    println!("Given '{serial}', found:");
+                    println!();
+                    for m in matches {
+                        println!("* name: '{}' serial: {:016X}", m.name, m.serial);
+                    }
+                    println!();
+                    bail!("Too many matches, be more specific!");
+                } else {
+                    serial = format!("{:016X}", matches[0].serial);
+                    serial_num = Some(matches[0].serial);
+                }
+            };
+
+            let Some(serial_num) = serial_num else {
+                bail!("Couldn't figure a serial number out!");
+            };
+
+            println!("{serial_num:016X}");
+            let schema = client
+                .get_device_schemas(serial_num)
+                .await
+                .unwrap()
+                .unwrap();
+
+            println!();
+            println!("# Endpoints for {serial}");
+            println!();
+            println!("## By path");
+            println!();
+
+            let longest_ep = schema.endpoints.iter().map(|e| e.path.len()).max().unwrap();
+            let longest_req = schema
+                .endpoints
+                .iter()
+                .map(|e| e.req_ty.name.len())
+                .max()
+                .unwrap_or(0)
+                .max("Request Type".len());
+            let longest_resp = schema
+                .endpoints
+                .iter()
+                .map(|e| e.resp_ty.name.len())
+                .max()
+                .unwrap_or(0)
+                .max("Response Type".len());
+
+            println!(
+                "| {:longest_ep$} | {:longest_req$} | {:longest_resp$} |",
+                "path", "Request Type", "Response Type"
+            );
+            println!(
+                "| {:-<longest_ep$} | {:-<longest_req$} | {:-<longest_resp$} |",
+                "", "", ""
+            );
+
+            let mut used_tys = HashSet::new();
+
+            for ep in schema.endpoints {
+                println!(
+                    "| {:longest_ep$} | {:longest_req$} | {:longest_resp$} |",
+                    ep.path, ep.req_ty.name, ep.resp_ty.name
+                );
+                discover_tys(&ep.req_ty, &mut used_tys);
+                discover_tys(&ep.resp_ty, &mut used_tys);
+            }
+            println!();
+            println!("## Type Definitions");
+            println!();
+            println!("Non-primitive types used by endpoints");
+
+            let mut tys: Vec<OwnedNamedType> = used_tys
+                .into_iter()
+                .filter(|ont| !is_prim(&ont.ty))
+                .collect();
+            tys.sort_by_key(|o| o.name.clone());
+
+            for ty in tys {
+                println!();
+                println!("### `{}`", ty.name);
+                println!();
+                println!("{}", ty.to_pseudocode());
+            }
+            println!();
+
+            Ok(())
+        }
+    }
+}
+
+async fn device_proxy(
+    client: SquadClient,
+    serial: String,
+    path: String,
+    message: String,
+) -> anyhow::Result<()> {
+    let serial = u64::from_str_radix(&serial, 16)?;
+    let msg = message.parse()?;
+
+    let res = client.proxy_endpoint_json(serial, &path, 0, msg).await;
+
+    match res {
+        Ok(v) => println!("Response: '{v}'"),
+        Err(e) => println!("Error: '{e}'"),
+    }
+
+    Ok(())
+}
+
+async fn device_cmds(client: SquadClient, device: &Device) -> anyhow::Result<()> {
+    let serial = u64::from_str_radix(&device.serial, 16)?;
+    let schema = client.get_device_schemas(serial).await.unwrap().unwrap();
+    match device.command {
+        DeviceCommands::Types => {
+            println!();
+            println!("Types used by device {}", device.serial);
+            println!();
+
+            let base = SchemaReport::default();
+            let uniq_tys = schema.types.difference(&base.types);
+
+            for ty in uniq_tys {
+                println!("* {ty}");
+            }
+            println!();
+            Ok(())
+        }
+        DeviceCommands::Endpoints => {
+            println!();
+            println!("Endpoints offered by device {}", device.serial);
+            println!();
+
+            for ep in schema.endpoints {
+                if ep.resp_ty.ty == OwnedDataModelType::Unit {
+                    println!("* '{}' => async fn({})", ep.path, ep.req_ty.name);
+                } else {
+                    println!(
+                        "* '{}' => async fn({}) -> {}",
+                        ep.path, ep.req_ty.name, ep.resp_ty.name
+                    );
+                }
+            }
+            println!();
+            Ok(())
+        }
+        DeviceCommands::TopicsOut => {
+            println!();
+            println!("Topics offered by device {}", device.serial);
+            println!();
+
+            for tp in schema.topics_out {
+                println!("* '{}' => Channel<{}>", tp.path, tp.ty.name);
+            }
+            println!();
+            Ok(())
+        }
+        DeviceCommands::TopicsIn => {
+            println!();
+            println!("Topics handled by device {}", device.serial);
+            println!();
+
+            for tp in schema.topics_in {
+                println!("* '{}' => Channel<{}>", tp.path, tp.ty.name);
+            }
+            println!();
+            Ok(())
+        }
+        DeviceCommands::Logs { count } => {
+            let count = count.unwrap_or(8);
+            let logs = client
+                .get_device_logs(serial, count)
+                .await
+                .unwrap()
+                .unwrap();
+
+            println!();
+            println!("Logs (last {} messages):", count.min(logs.len() as u32));
+            println!();
+            for log in logs {
+                println!("* {} => {}", log.uuidv7.id_to_time().time(), log.msg);
+            }
+            println!();
+            Ok(())
+        }
+    }
+}
---
+++ tools/poststation-sdk/src/lib.rs
@@ -0,0 +1,329 @@
+use core::fmt::Debug;
+use std::{error::Error, fmt::Display, future::Future, net::SocketAddr};
+
+use postcard_dyn::Value;
+use postcard_rpc::{
+    host_client::{HostClient, SchemaReport, WireRx, WireSpawn, WireTx},
+    standard_icd::{PingEndpoint, WireError, ERROR_PATH},
+};
+use poststation_api_icd::{
+    DeviceData, GetDevicesEndpoint, GetLogsEndpoint, GetSchemasEndpoint, GetTopicsEndpoint, Log, LogRequest,
+    ProxyEndpoint, ProxyRequest, ProxyResponse, TopicMsg, TopicRequest, Uuidv7,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpStream,
+    },
+};
+
+pub use postcard_schema as schema;
+
+// ---
+
+pub struct SquadClient {
+    client: HostClient<WireError>,
+}
+
+impl SquadClient {
+    pub fn raw_client(&self) -> &HostClient<WireError> {
+        &self.client
+    }
+
+    pub async fn get_devices(&self) -> Result<Vec<DeviceData>, ()> {
+        self.client.send_resp::<GetDevicesEndpoint>(&()).await.map_err(drop)
+    }
+
+    pub async fn get_device_schemas(&self, serial: u64) -> Result<Option<SchemaReport>, ()> {
+        let res = self
+            .client
+            .send_resp::<GetSchemasEndpoint>(&serial)
+            .await
+            .map_err(drop)?;
+        let Some(res) = res else {
+            return Ok(None);
+        };
+        Ok(Some(res))
+    }
+
+    pub async fn get_device_logs(&self, serial: u64, count: u32) -> Result<Option<Vec<Log>>, ()> {
+        self.client
+            .send_resp::<GetLogsEndpoint>(&LogRequest { serial, count })
+            .await
+            .map_err(drop)
+    }
+
+    pub async fn get_device_topics_out_by_path_raw(
+        &self,
+        serial: u64,
+        path: &str,
+        count: u32,
+    ) -> Result<Option<Vec<TopicMsg>>, ()> {
+        let schemas = self.get_device_schemas(serial).await?;
+        let Some(schemas) = schemas else {
+            return Ok(None);
+        };
+
+        // find key
+        let res = schemas
+            .topics_out
+            .iter()
+            .find(|t| t.path.as_str() == path)
+            .map(|t| t.key);
+        let Some(key) = res else { return Ok(None) };
+
+        self.client
+            .send_resp::<GetTopicsEndpoint>(&TopicRequest {
+                serial,
+                count,
+                path: path.to_string(),
+                key,
+            })
+            .await
+            .map_err(drop)
+    }
+
+    pub async fn get_device_topics_out_by_path_json(
+        &self,
+        serial: u64,
+        path: &str,
+        count: u32,
+    ) -> Result<Option<Vec<(Uuidv7, Value)>>, ()> {
+        let schemas = self.get_device_schemas(serial).await?;
+        let Some(schemas) = schemas else {
+            return Ok(None);
+        };
+
+        // find key
+        let res = schemas.topics_out.iter().find(|t| t.path.as_str() == path);
+        let Some(schema) = res else { return Ok(None) };
+
+        let raws = self
+            .client
+            .send_resp::<GetTopicsEndpoint>(&TopicRequest {
+                serial,
+                count,
+                path: path.to_string(),
+                key: schema.key,
+            })
+            .await
+            .map_err(drop)?;
+        let Some(raws) = raws else {
+            return Ok(None);
+        };
+
+        let res = raws
+            .into_iter()
+            .map(|tm| {
+                let msg = postcard_dyn::from_slice_dyn(&schema.ty, &tm.msg).map_err(drop)?;
+                Ok((tm.uuidv7, msg))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Some(res))
+    }
+
+    pub async fn proxy_endpoint_json(
+        &self,
+        serial: u64,
+        path: &str,
+        seq_no: u32,
+        body: Value,
+    ) -> Result<Value, String> {
+        let Ok(Some(schemas)) = self.get_device_schemas(serial).await else {
+            return Err("endpoint not found".into());
+        };
+
+        // find key
+        let res = schemas.endpoints.iter().find(|e| e.path.as_str() == path);
+        let Some(schema) = res else {
+            return Err("endpoint not found".into());
+        };
+
+        let Ok(body) = postcard_dyn::to_stdvec_dyn(&schema.req_ty, &body) else {
+            todo!()
+        };
+        let req = ProxyRequest {
+            serial,
+            path: schema.path.clone(),
+            req_key: schema.req_key,
+            resp_key: schema.resp_key,
+            seq_no,
+            req_body: body,
+        };
+
+        let resp = self.client.send_resp::<ProxyEndpoint>(&req).await;
+
+        let resp = match resp {
+            Ok(r) => r,
+            Err(e) => {
+                return Err(format!("Server error: {e:?}"));
+            }
+        };
+
+        let resp = match resp {
+            ProxyResponse::Ok { body, .. } => body,
+            ProxyResponse::WireErr { body, .. } => return Err(format!("WireErr: {body:?}")),
+            ProxyResponse::OtherErr(e) => return Err(format!("Other Server Err: '{e}'")),
+        };
+
+        let resp = postcard_dyn::from_slice_dyn(&schema.resp_ty, &resp);
+
+        match resp {
+            Ok(v) => Ok(v),
+            Err(e) => Err(format!("Decode error: '{e:?}'")),
+        }
+    }
+}
+
+pub async fn connect<T: tokio::net::ToSocketAddrs>(addr: T) -> SquadClient {
+    let socket = TcpStream::connect(addr).await.unwrap();
+    let addr = socket.peer_addr().unwrap();
+    socket.set_nodelay(true).unwrap();
+    let (rx, tx) = socket.into_split();
+
+    let client = HostClient::<WireError>::new_with_wire(
+        TcpCommsTx { tx },
+        TcpCommsRx {
+            rx,
+            addr,
+            buf: vec![],
+        },
+        TcpSpawn,
+        postcard_rpc::header::VarSeqKind::Seq4,
+        ERROR_PATH,
+        64,
+    );
+
+    let res = client.send_resp::<PingEndpoint>(&42).await.unwrap();
+    assert_eq!(res, 42);
+
+    SquadClient { client }
+}
+
+pub enum TcpCommsRxError {
+    RxOverflow,
+    ConnError,
+}
+
+impl Debug for TcpCommsRxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("oops")
+    }
+}
+
+impl Display for TcpCommsRxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("oops")
+    }
+}
+
+impl Error for TcpCommsRxError {}
+
+struct TcpCommsRx {
+    addr: SocketAddr,
+    buf: Vec<u8>,
+    rx: OwnedReadHalf,
+}
+
+impl TcpCommsRx {
+    async fn receive_inner(&mut self) -> Result<Vec<u8>, TcpCommsRxError> {
+        let mut rx_buf = [0u8; 1024];
+        'frame: loop {
+            if self.buf.len() > (1024 * 1024) {
+                tracing::warn!(?self.addr, "Refusing to collect >1MiB, terminating");
+                self.buf.clear();
+                return Err(TcpCommsRxError::RxOverflow);
+            }
+
+            // Do we have a message already?
+            if let Some(pos) = self.buf.iter().position(|b| *b == 0) {
+                // we found the end of a message, attempt to decode it
+                let mut split = self.buf.split_off(pos + 1);
+                core::mem::swap(&mut self.buf, &mut split);
+
+                // Can we decode the cobs?
+                let res = cobs::decode_vec(&split);
+                let Ok(msg) = res else {
+                    tracing::warn!(?self.addr, discarded = split.len(), "Discarding bad message (cobs)");
+                    continue 'frame;
+                };
+
+                return Ok(msg);
+            }
+
+            // No message yet, let's try and receive some data
+            let Ok(used) = self.rx.read(&mut rx_buf).await else {
+                tracing::warn!(?self.addr, "Closing");
+                return Err(TcpCommsRxError::ConnError);
+            };
+            if used == 0 {
+                tracing::warn!(?self.addr, "Closing");
+                return Err(TcpCommsRxError::ConnError);
+            }
+            self.buf.extend_from_slice(&rx_buf[..used]);
+        }
+    }
+}
+
+impl WireRx for TcpCommsRx {
+    type Error = TcpCommsRxError;
+
+    fn receive(&mut self) -> impl Future<Output = Result<Vec<u8>, Self::Error>> + Send {
+        self.receive_inner()
+    }
+}
+
+// ---
+
+pub enum TcpCommsTxError {
+    CommsError,
+}
+
+impl Debug for TcpCommsTxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("oops")
+    }
+}
+
+impl Display for TcpCommsTxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("oops")
+    }
+}
+
+impl Error for TcpCommsTxError {}
+
+struct TcpCommsTx {
+    tx: OwnedWriteHalf,
+}
+
+impl TcpCommsTx {
+    async fn send_inner(&mut self, data: Vec<u8>) -> Result<(), TcpCommsTxError> {
+        let mut data = cobs::encode_vec(&data);
+        data.push(0);
+        self.tx
+            .write_all(&data)
+            .await
+            .map_err(|_| TcpCommsTxError::CommsError)
+    }
+}
+
+impl WireTx for TcpCommsTx {
+    type Error = TcpCommsTxError;
+
+    fn send(&mut self, data: Vec<u8>) -> impl Future<Output = Result<(), Self::Error>> + Send {
+        self.send_inner(data)
+    }
+}
+
+// ---
+
+struct TcpSpawn;
+
+impl WireSpawn for TcpSpawn {
+    fn spawn(&mut self, fut: impl Future<Output = ()> + Send + 'static) {
+        tokio::spawn(fut);
+    }
+}
