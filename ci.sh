3f2e2469b3ac932d5fa779cb92308e2c342afce8
James Munns <james@onevariable.com>
2024-12-17 14:24:38 +0100

    check instead of build for lib crates

--- ci.sh
+++ ci.sh
@@ -7,17 +7,17 @@ rustup target add \
     thumbv6m-none-eabi
 
 # API ICD
-cargo build \
+cargo check \
     --manifest-path crates/poststation-api-icd/Cargo.toml \
     --no-default-features \
     --profile ci
-cargo build \
+cargo check \
     --manifest-path crates/poststation-api-icd/Cargo.toml \
     --all-features \
     --profile ci
 
 # SDK crate
-cargo build \
+cargo check \
     --manifest-path tools/poststation-sdk/Cargo.toml \
     --profile ci
 
@@ -29,12 +29,12 @@ cargo build \
 ## Templates
 
 # ICD
-cargo build \
+cargo check \
     --manifest-path templates/icd/Cargo.toml \
     --features="use-std" \
     --profile ci
 
-cargo build \
+cargo check \
     --manifest-path  templates/icd/Cargo.toml \
     --no-default-features \
     --target thumbv6m-none-eabi \
