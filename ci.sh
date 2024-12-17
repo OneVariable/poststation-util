cc2cf87b2e197ee598736dc213f11125a1db784c
James Munns <james@onevariable.com>
2024-12-17 14:27:51 +0100

    Add basic CI checks (#2)
    * Add basic CI check
    * Optimize CI a bit
    * check instead of build for lib crates

---
+++ ci.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# Template builds for RP2040/cortex-m0+
+rustup target add \
+    thumbv6m-none-eabi
+
+# API ICD
+cargo check \
+    --manifest-path crates/poststation-api-icd/Cargo.toml \
+    --no-default-features \
+    --profile ci
+cargo check \
+    --manifest-path crates/poststation-api-icd/Cargo.toml \
+    --all-features \
+    --profile ci
+
+# SDK crate
+cargo check \
+    --manifest-path tools/poststation-sdk/Cargo.toml \
+    --profile ci
+
+# CLI tool
+cargo build \
+    --manifest-path tools/poststation-cli/Cargo.toml \
+    --profile ci
+
+## Templates
+
+# ICD
+cargo check \
+    --manifest-path templates/icd/Cargo.toml \
+    --features="use-std" \
+    --profile ci
+
+cargo check \
+    --manifest-path  templates/icd/Cargo.toml \
+    --no-default-features \
+    --target thumbv6m-none-eabi \
+    --profile ci
+
+# RP2040
+cargo build \
+    --manifest-path  templates/rp2040/Cargo.toml \
+    --target thumbv6m-none-eabi \
+    --profile ci
